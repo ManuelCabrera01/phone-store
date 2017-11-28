f8eb18b4d58545da1037d7e5667d405491b5b39e
Manuel <m.a.c.p90@gmail.com>
2017-11-27 19:06:52 -0500

    multer instale

--- app.js
+++ app.js
@@ -30,7 +30,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+
 app.use('/api', phonesApi);
+// This will be the default route is nothing else is caught
+app.use(function(req, res) {
+  res.sendfile(__dirname + '/public/index.html');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
